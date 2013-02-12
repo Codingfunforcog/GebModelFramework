21f8672edc41e1fb359986fac08ac044f20e874d
abhinavvaid <abhinav.vaid@yahoo.com>
2013-02-12 12:57:46 +0530

    Removing unnecessary file changing the locator

--- src/Pages/HomePageO2.groovy
+++ src/Pages/HomePageO2.groovy
@@ -5,7 +5,7 @@ class HomePageO2 extends Page {
     static url = "http://shop.o2.co.uk/home"
     static at = { title == "O2 Shop - Home" }
     static content = {
-        businessPhones { $("a", title: "View best business deals from O2") }
+        businessPhones { $("a", text: "Business") }
 
     }
 }
--- src/test/groovy/test1.groovy
+++
@@ -1,7 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: CTS1
- * Date: 1/30/13
- * Time: 3:56 PM
- * To change this template use File | Settings | File Templates.
- */
