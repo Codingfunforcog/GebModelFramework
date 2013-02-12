85bc82f5bf8fe263e5640233a62e86346bd8ab7d
abhinavvaid <abhinav.vaid@yahoo.com>
2013-02-12 13:12:20 +0530

    Adding GebConfig.groovy

---
+++ src/resources/GebConfig.groovy
@@ -0,0 +1,21 @@
+/*
+	This is the Geb configuration file.
+	
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
+
+import org.openqa.selenium.chrome.ChromeDriver
+
+reportsDir = "target/geb-reports"
+baseUrl = "http://shop.o2.co.uk/home"
+def userDirectory = System.getProperty("user.dir")
+System.setProperty("webdriver.chrome.driver", userDirectory + "\\src\\resources\\chromedriver.exe");
+driver = { new ChromeDriver() }
+
+environments {
+
+    chrome {
+        driver = { new ChromeDriver() }
+    }
+
+}
